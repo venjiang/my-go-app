267fa43b9c7399cd9e1047866c09705ba34ab033
venjiang <venjiang@gmail.com>
2022-06-17 17:07:03 +0800

    init

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var addr string = ":8080"
+
+func init() {
+	if v := os.Getenv("ADDR"); v != "" {
+		addr = v
+	}
+}
+
+func main() {
+	e := gin.Default()
+	e.GET("/", func(c *gin.Context) {
+		c.String(200, "Gin Web App on Cloudflare @%v", time.Now().UnixMilli())
+	})
+	fmt.Println("Gin Web App running @", addr)
+	e.Run(addr)
+}
